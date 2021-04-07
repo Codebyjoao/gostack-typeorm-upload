8d092909585dffe17a77e89086243b3d92b2e096
Joao <joaovictor.sgoncalves@hotmail.com>
2021-04-07 19:21:22 -0300

    new commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,25 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transaction = await this.find();
+
+    const{income, outcome} = transaction.reduce((acumulator, transaction) => {
+      if(transaction.type == 'income'){
+        acumulator.income += Number(transaction.value);
+      }else if(transaction.type == 'outcome'){
+        acumulator.outcome += Number(transaction.value);
+      }
+      return acumulator;
+    },
+    {
+      income: 0,
+      outcome:0,
+      total: 0
+    });
+
+    const total = income - outcome;
+
+    return {income, outcome, total}
   }
 }
 
